b490206e731b37e8e6c264c002b8b385384bd504
jpotth <jpotth@umich.edu>
2016-04-02 19:21:09 -0400

    change

--- gameRegion.m
+++ gameRegion.m
@@ -2,15 +2,15 @@
 %This would be a good starting point from my point of view
 %Problem now is that it does not detect yellow dots on the dominos
 
-image1 = fullfile('data','game_picture1.jpg');
+image1 = fullfile('data','Domino_game.jpg');
 imdata = imread(image1);
 imdata = rgb2gray(imdata);
 
 imdata = 255 - imdata;
 
-BW = imbinarize(imdata,'adaptive','ForegroundPolarity','dark','Sensitivity',0.55);
+BW = imbinarize(imdata,'adaptive','ForegroundPolarity','dark','Sensitivity',0.57);
 
-e = imfill(BW,'holes');
+e = imfill(BW,[3 3], 8);
 
 f = bwlabel(e);
 
