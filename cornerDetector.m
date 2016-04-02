ffdb769d84a4fdffddb3b9c8ba544de42b08c5fb
Yeshwanth <Devabhaktuni@0587378025.wireless.umich.net>
2016-04-02 19:38:56 -0400

    some more changes

--- cornerDetector.m
+++ cornerDetector.m
@@ -2,19 +2,11 @@
 %This would be a good starting point from my point of view
 %Problem now is that it does not detect yellow dots on the dominos
 
-image1 = fullfile('data','game_picture1.jpg');
-imdata = imread(image1);
-imdata = rgb2gray(imdata);
+function [corners] = cornerDetector(region)
+    
+imshow(region);
+corners = region;
 
-disp(size(imdata));
 
-imdata = 255 - imdata;
 
-BW = imbinarize(imdata,'adaptive','ForegroundPolarity','dark','Sensitivity',0.55);
-
-e = imfill(BW,'holes');
-
-f = bwlabel(e);
-
-imshow(f);
-
+end
--- main.m
+++ main.m
@@ -1,2 +1,9 @@
-gameRegion
+%This is the main function where all the parts of the project is put
+%togethor.
+%P.S : Please read the comments on top of each of the functions called as
+%it is a detailed walkthrough of what exactly it is that is being done so
+%that the image is set up for the next function
 
+region = gameRegion;
+
+corners = cornerDetector(region);
